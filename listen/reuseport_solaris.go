ccdfa3024902c01d4300d747f8f49c56d3e3b0b1
Evert Joubert <evocert@gmail.com>
2024-01-10 12:05:09 +0200

    correct go:build filter between solaris and illumos

--- listen/reuseport_solaris.go
+++ listen/reuseport_solaris.go
@@ -1,3 +1,6 @@
+//go:build !illumos
+// +build !illumos
+
 package listen
 
 import (
@@ -12,21 +15,22 @@ var listenConfig = net.ListenConfig{
 	Control: func(network, address string, c syscall.RawConn) (err error) {
 		return c.Control(func(fd uintptr) {
 			if err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1); err == nil {
-				if err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1); err == nil {
-					if err = syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, syscall.TCP_NODELAY, 1); err == nil {
-						if err = syscall.SetNonblock(int(fd), true); err == nil {
-							if err = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_RCVBUF, 32768*2); err == nil {
-								err = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_SNDBUF, 32768*2)
-							}
+				//if err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1); err == nil {
+				if err = syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, syscall.TCP_NODELAY, 1); err == nil {
+					if err = syscall.SetNonblock(int(fd), true); err == nil {
+						if err = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_RCVBUF, 32768*2); err == nil {
+							err = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_SNDBUF, 32768*2)
 						}
 					}
 				}
+				//}
 			}
 		})
 	},
 }
 
-// Listen returns a TCP listener with the SO_REUSEADDR and SO_REUSEPORT options set.
+// Listen returns TCP listener with SO_REUSEADDR option set, SO_REUSEPORT is not supported on Windows, so it uses
+// SO_REUSEADDR as an alternative to achieve the same effect.
 func Listen(network, addr string) (ln net.Listener, err error) {
 	ln, err = listenConfig.Listen(context.Background(), network, addr)
 	return
