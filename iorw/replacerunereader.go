d29b0b3e3f7ed4dc83e9b01a62feb77bbc684115
Evert Joubert <evocert@gmail.com>
2024-04-28 10:15:31 +0200

    Re-impement replace process

--- iorw/replacerunereader.go
+++ iorw/replacerunereader.go
@@ -8,9 +8,12 @@ import (
 )
 
 type ReplaceRuneReader struct {
+	rnstst    []rune
+	rmnrnsl   int
 	orgrdr    *RuneReaderSlice
-	org, crnt bool
 	crntrdr   io.RuneReader
+	crntrns   []rune
+	crntrnsl  int
 	rplcewith map[string]interface{}
 	rplcekeys []string
 	mxlrplcL  int
@@ -24,7 +27,7 @@ func NewReplaceRuneReader(orgrdr interface{}, rplwiths ...interface{}) (rplcerrd
 			orgrnrdr = bufio.NewReaderSize(orgr, 1)
 		}
 	}
-	rplcerrdr = &ReplaceRuneReader{orgrdr: NewRuneReaderSlice(orgrnrdr)}
+	rplcerrdr = &ReplaceRuneReader{orgrdr: NewRuneReaderSlice(orgrnrdr), rnstst: make([]rune, 8192)}
 	rplwithsl := len(rplwiths)
 	for rplwithsl > 0 && rplwithsl%2 == 0 {
 		rplcerrdr.ReplaceWith(rplwiths[0], rplwiths[1])
@@ -49,35 +52,76 @@ func (rplcerrdr *ReplaceRuneReader) ReplaceWith(phrase, replacewith interface{})
 			for rplckey := range rplcerrdr.rplcewith {
 				rplcerrdr.rplcekeys = append(rplcerrdr.rplcekeys, rplckey)
 			}
-			sort.Strings(rplcerrdr.rplcekeys)
-		}
-	}
-}
-
-func replacedWithReader(rplcewith map[string]interface{}, phrase string, isrepeatable bool) (rdr io.RuneReader) {
-	if len(rplcewith) > 0 && phrase != "" {
-		if phrsev, phrsok := rplcewith[phrase]; phrsok {
-			if phrss, phrssok := phrsev.(string); phrssok {
-				rdr = strings.NewReader(phrss)
-			} else if phrsr, _ := phrsev.(io.Reader); phrsr != nil {
-				if rdr, _ = phrsr.(io.RuneReader); rdr == nil {
-					rdr = bufio.NewReader(phrsr)
-				}
-			} else if phrsbf, _ := phrsev.(*Buffer); phrsbf != nil {
-				if isrepeatable {
-					rdr = phrsbf.Clone(true).Reader(true)
-				} else {
-					rdr = phrsbf.Reader()
-				}
+			if len(rplcerrdr.rplcekeys) > 0 {
+				sort.Slice(rplcerrdr.rplcekeys, func(i, j int) bool {
+					return len(rplcerrdr.rplcekeys[i]) < len(rplcerrdr.rplcekeys[j])
+				})
 			}
 		}
 	}
-	return
+}
+
+func replacedWithReader(rplcerrdr *ReplaceRuneReader, rplcewith map[string]interface{}, phrase string, isrepeatable bool, rnstopreappend ...rune) bool {
+	if len(rplcewith) > 0 && phrase != "" {
+		var preappndrdr io.RuneReader = nil
+		if len(rnstopreappend) > 0 {
+			preappndrdr = strings.NewReader(string(rnstopreappend))
+		}
+		if phrsev, phrsok := rplcewith[phrase]; phrsok {
+			if phrss, phrssok := phrsev.(string); phrssok && phrss != "" {
+				if preappndrdr != nil {
+					rplcerrdr.crntrdr = NewRuneReaderSlice(preappndrdr, strings.NewReader(phrss))
+					return true
+				}
+				rplcerrdr.crntrdr = strings.NewReader(phrss)
+				return true
+			}
+			if phrsr, _ := phrsev.(io.Reader); phrsr != nil {
+				if rdr, _ := phrsr.(io.RuneReader); rdr == nil {
+					if preappndrdr != nil {
+						rplcerrdr.crntrdr = NewRuneReaderSlice(preappndrdr, bufio.NewReader(phrsr))
+						return true
+					}
+					rplcerrdr.crntrdr = bufio.NewReader(phrsr)
+					return true
+				}
+				if preappndrdr != nil {
+					rplcerrdr.crntrdr = NewRuneReaderSlice(preappndrdr, bufio.NewReader(phrsr))
+					return true
+				}
+				rplcerrdr.crntrdr, _ = phrsr.(io.RuneReader)
+				return true
+			}
+			if phrsbf, _ := phrsev.(*Buffer); phrsbf != nil {
+				if isrepeatable {
+					if preappndrdr != nil {
+						rplcerrdr.crntrdr = NewRuneReaderSlice(preappndrdr, phrsbf.Clone(true).Reader(true))
+						return true
+					}
+					rplcerrdr.crntrdr = phrsbf.Clone(true).Reader(true)
+					return true
+				}
+				if preappndrdr != nil {
+					rplcerrdr.crntrdr = NewRuneReaderSlice(preappndrdr, phrsbf.Reader())
+					return true
+				}
+				rplcerrdr.crntrdr = phrsbf.Reader()
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func underlineReadRune(rplcerrdr *ReplaceRuneReader) (r rune, size int, err error) {
-	if rplcerrdr.crnt = (rplcerrdr.crntrdr != nil); rplcerrdr.crnt {
-		r, size, err = rplcerrdr.crntrdr.ReadRune()
+	if rplcerrdr.crntrnsl > 0 {
+		r, size, err = rplcerrdr.crntrns[0], 1, nil
+		rplcerrdr.crntrns = rplcerrdr.crntrns[1:]
+		rplcerrdr.crntrnsl--
+		return
+	}
+	if crntrdr := rplcerrdr.crntrdr; crntrdr != nil {
+		r, size, err = crntrdr.ReadRune()
 		if size == 0 && err == io.EOF {
 			if rplcerrdr.crntrdr == rplcerrdr.orgrdr {
 				rplcerrdr.orgrdr = nil
@@ -86,25 +130,88 @@ func underlineReadRune(rplcerrdr *ReplaceRuneReader) (r rune, size int, err erro
 			r, size, err = underlineReadRune(rplcerrdr)
 			return
 		}
-		if err == io.EOF {
-			rplcerrdr.crntrdr = nil
-			if rplcerrdr.crntrdr == rplcerrdr.orgrdr {
-				rplcerrdr.orgrdr = nil
-				rplcerrdr.crnt = false
-				rplcerrdr.org = false
-			} else {
-				err = nil
-			}
-			return
-		}
-	} else if rplcerrdr.org = (rplcerrdr.orgrdr != nil); rplcerrdr.org {
-		if r, size, err = rplcerrdr.orgrdr.ReadRune(); size == 0 {
-			rplcerrdr.orgrdr = nil
-			rplcerrdr.org = false
-		}
 		return
 	}
-	if size == 0 {
+	if len(rplcerrdr.rplcekeys) == 0 && rplcerrdr.orgrdr != nil {
+		r, size, err = rplcerrdr.orgrdr.ReadRune()
+		return
+	}
+	if orgrdr, rmngl := rplcerrdr.orgrdr, rplcerrdr.rmnrnsl; orgrdr != nil || rmngl > 0 {
+		var rnstst []rune = rplcerrdr.rnstst
+		for err == nil {
+			rnststL := rmngl
+			if rplcerrdr.orgrdr != nil {
+				for rnsi := range rnstst {
+					if r, size, err = rplcerrdr.orgrdr.ReadRune(); size > 0 && (err == io.EOF || err == nil) {
+						rnstst[rnsi+rmngl] = r
+						rnststL += (rmngl + 1)
+						continue
+					}
+					if err != nil {
+						if err != io.EOF {
+							return
+						}
+					}
+					break
+				}
+			}
+
+			if rnststL > 0 {
+				lstphrn := -1
+				lstphrsi := -1
+				for prsn, prsphrase := range rplcerrdr.rplcekeys {
+					if prsn > lstphrn {
+						if phrsi := IndexOfRunes(rnstst[:rnststL], []rune(prsphrase)...); (phrsi > -1 && lstphrsi == -1) || (phrsi > -1 && phrsi < lstphrsi) {
+							subphrsn := -1
+							for subn, sbphrs := range rplcerrdr.rplcekeys[prsn+1:] {
+								if !RunesHasPrefix([]rune(sbphrs), []rune(prsphrase)...) {
+									if sbphrsi := IndexOfRunes(rnstst[:rnststL], []rune(sbphrs)...); sbphrsi < phrsi && (sbphrsi > -1 && lstphrsi == -1) || (sbphrsi > -1 && phrsi < lstphrsi) {
+										lstphrsi = sbphrsi
+										lstphrn = subn + prsn + 1
+										subphrsn = subn
+										break
+									}
+									continue
+								}
+								if sbphrsi := IndexOfRunes(rnstst[:rnststL], []rune(sbphrs)...); sbphrsi < phrsi && (sbphrsi > -1 && lstphrsi == -1) || (sbphrsi > -1 && phrsi < lstphrsi) {
+									lstphrsi = sbphrsi
+									lstphrn = subn + prsn + 1
+									subphrsn = subn
+									break
+								}
+							}
+							if subphrsn > -1 {
+								continue
+							}
+							if lstphrsi == -1 || lstphrsi > phrsi {
+								lstphrsi = phrsi
+								lstphrn = prsn
+							}
+						}
+					}
+				}
+				if lstphrn == -1 {
+					rplcerrdr.rmnrnsl = 0
+					rplcerrdr.crntrdr = strings.NewReader(string(rnstst[:rnststL]))
+					//rplcerrdr.crntrns = append(rplcerrdr.crntrns, rnstst[:rnststL]...)
+					//rplcerrdr.crntrnsl += rnststL
+					return underlineReadRune(rplcerrdr)
+				}
+				mtchdprhase := rplcerrdr.rplcekeys[lstphrn]
+				var preappendrunes []rune = nil
+				if lstphrsi > 0 {
+					//rplcerrdr.crntrns = append(rplcerrdr.crntrns, rnstst[:lstphrsi]...)
+					//rplcerrdr.crntrnsl += len(rnstst[:lstphrsi])
+					preappendrunes = make([]rune, len(rnstst[:lstphrsi]))
+					copy(preappendrunes, rnstst[:lstphrsi])
+				}
+				rplcerrdr.rmnrnsl = copy(rnstst, rnstst[lstphrsi+len(mtchdprhase):rnststL])
+				replacedWithReader(rplcerrdr, rplcerrdr.rplcewith, mtchdprhase, true, preappendrunes...)
+				return underlineReadRune(rplcerrdr)
+			}
+		}
+	}
+	if size == 0 && err == nil {
 		err = io.EOF
 	}
 	return
@@ -112,77 +219,7 @@ func underlineReadRune(rplcerrdr *ReplaceRuneReader) (r rune, size int, err erro
 
 func (rplcerrdr *ReplaceRuneReader) ReadRune() (r rune, size int, err error) {
 	if rplcerrdr != nil {
-		if rplcerrdr.mxlrplcL == 0 {
-			if rplcerrdr.orgrdr != nil {
-				if r, size, err = rplcerrdr.orgrdr.ReadRune(); size == 0 && err == nil {
-					err = io.EOF
-				}
-			}
-			return
-		}
-		for err == nil {
-			if r, size, err = underlineReadRune(rplcerrdr); rplcerrdr.crnt || (!rplcerrdr.crnt && !rplcerrdr.org) {
-				return
-			}
-			if rplcerrdr.mxlrplcL > 0 && size > 0 && (rplcerrdr.org) {
-				mtcdids := map[int]bool{}
-				for rpi, rplk := range rplcerrdr.rplcekeys {
-					if !mtcdids[rpi] {
-						if rplk[0:1] == string(r) {
-							mtcdids[rpi] = true
-						}
-					}
-				}
-				if len(mtcdids) == 0 {
-					return
-				}
-				txtrns := make([]rune, rplcerrdr.mxlrplcL)
-				for rplci := range rplcerrdr.mxlrplcL {
-					txtrns[rplci] = r
-					for rpi, rplk := range rplcerrdr.rplcekeys {
-						if !mtcdids[rpi] {
-							if len(rplk) > rplci && rplk[0:rplci+1] == string(txtrns[0:rplci+1]) {
-								mtcdids[rpi] = true
-							}
-						} else if mtcdids[rpi] && len(rplk) >= (rplci+1) && rplk[0:rplci+1] != string(txtrns[0:rplci+1]) {
-							delete(mtcdids, rpi)
-						}
-					}
-					if len(mtcdids) == 0 {
-						rplcerrdr.crntrdr = strings.NewReader(string(txtrns[:rplci+1]))
-						clear(txtrns)
-						r, size, err = underlineReadRune(rplcerrdr)
-						return
-					} else {
-						if rplci < (rplcerrdr.mxlrplcL - 1) {
-							if r, size, err = rplcerrdr.orgrdr.ReadRune(); size > 0 && (err == io.EOF || err == nil) {
-								if err == io.EOF {
-									err = nil
-								}
-							} else {
-								break
-							}
-						} else {
-							if len(mtcdids) == 1 {
-								for fndid, _ := range mtcdids {
-									rplcerrdr.crntrdr = replacedWithReader(rplcerrdr.rplcewith, rplcerrdr.rplcekeys[fndid], true)
-									clear(txtrns)
-									r, size, err = underlineReadRune(rplcerrdr)
-									return
-								}
-							} else {
-								rplcerrdr.crntrdr = strings.NewReader(string(txtrns[:rplci+1]))
-								clear(txtrns)
-								r, size, err = underlineReadRune(rplcerrdr)
-								return
-							}
-						}
-					}
-				}
-				continue
-			}
-			break
-		}
+		r, size, err = underlineReadRune(rplcerrdr)
 	}
 	if size == 0 && err == nil {
 		err = io.EOF
