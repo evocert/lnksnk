512c4672bb21d2486a3e96390e04c92f45f0b8fd
Evert Joubert <evocert@gmail.com>
2024-04-28 11:03:59 +0200

    rename func vmParseEval to ParseEval. improve invoking interval vm (goja.Runtime) wrapper. Fixed detection and formatting of an active content request path

--- serve/requestor.go
+++ serve/requestor.go
@@ -74,31 +74,17 @@ func ProcessRequest(path string, httprqst *http.Request, httprspns http.Response
 
 var fs = resources.GLOBALRSNG().FS()
 
-func vmParseEval(vm *active.VM, path, pathext string, pathmodified time.Time, Out io.Writer, In io.Reader, fs *fsutils.FSUtils, invertactive bool, fi fsutils.FileInfo, fnmodified func(modified time.Time), fnactiveraw func(rsraw bool, rsactive bool)) (err error) {
-	if fi != nil {
-		if canprs, canprserr := parsing.CanParse(true, pathmodified, path, "", pathext, Out, fs, invertactive, func(atvrdr *iorw.BuffReader, cdepgrm func() interface{}, setcdepgrm func(interface{})) (atverr error) {
-			if _, atverr = vm.Eval(atvrdr, cdepgrm, setcdepgrm); atverr != nil {
-				atverr = nil
-			}
-			return
-		}); (canprs && canprserr == nil) || canprserr != nil {
-			if f, ferr := fi.Open(fnactiveraw, fnmodified); ferr == nil {
-				err = parsing.Parse(false, true, pathmodified, path, "", pathext, Out, f, fs, invertactive, func(atvrdr *iorw.BuffReader, cdepgrm func() interface{}, setcdepgrm func(interface{})) (atverr error) {
-					if _, atverr = vm.Eval(atvrdr, cdepgrm, setcdepgrm); atverr != nil {
-						atverr = nil
-					}
-					return
-				})
-			}
-		}
-	} else if In != nil {
-		err = parsing.Parse(false, true, pathmodified, path, "", pathext, Out, In, fs, invertactive, func(atvrdr *iorw.BuffReader, cdepgrm func() interface{}, setcdepgrm func(interface{})) (atverr error) {
-			if _, atverr = vm.Eval(atvrdr, cdepgrm, setcdepgrm); atverr != nil {
-				atverr = nil
-			}
-			return
-		})
+func ParseEval(evalcode func(a ...interface{}) (val interface{}, err error), path, pathext string, pathmodified time.Time, Out io.Writer, In io.Reader, fs *fsutils.FSUtils, invertactive bool, fi fsutils.FileInfo, fnmodified func(modified time.Time), fnactiveraw func(rsraw bool, rsactive bool)) (err error) {
+	if fi != nil && path != "" && path[0:1] != "/" {
+		path += fi.Path()
 	}
+	err = parsing.Parse(true, pathmodified, path, pathext, Out, func() (f io.Reader, ferr error) {
+		if fi != nil {
+			f, ferr = fi.Open(fnactiveraw, fnmodified)
+			return
+		}
+		return In, nil
+	}, fs, invertactive, evalcode)
 	return
 }
 
@@ -135,207 +121,234 @@ func internalServeRequest(path string, In *reader, Out *writer, httpw http.Respo
 	var dbhnlr *database.DBMSHandler = DBMS.DBMSHandler(ctx, active.RuntimeFunc(func(functocall interface{}, args ...interface{}) interface{} {
 		return vm.InvokeFunction(functocall, args...)
 	}), params, CHACHING, fs, func(ina ...interface{}) (a []interface{}) {
-		if vm != nil {
-			stmntoutbuf := iorw.NewBuffer()
-			defer stmntoutbuf.Close()
-			stmntoutbuf.Print(ina...)
-			vmw := vm.W
-			vm.W = stmntoutbuf
-			vmParseEval(vm, ":no-cache/", ".js", time.Now(), stmntoutbuf, stmntoutbuf.Clone(true).Reader(true), fs, false, nil, nil, nil)
-			vm.W = vmw
-			a = append(a, stmntoutbuf.Clone(true).Reader(true))
+		if vm != nil && len(ina) == 1 {
+			if fia, _ := ina[0].(fsutils.FileInfo); fia != nil {
+				stmntoutbuf := iorw.NewBuffer()
+				defer stmntoutbuf.Close()
+				vmw := vm.W
+				vm.W = stmntoutbuf
+				if evalerr := ParseEval(vm.Eval, fia.Path(), fia.PathExt(), fia.ModTime(), stmntoutbuf, nil, fs, false, fia, nil, nil); evalerr == nil {
+					a = append(a, stmntoutbuf.Clone(true).Reader(true))
+				}
+				vm.W = vmw
+			}
 		} else {
 			a = append(a, ina...)
 		}
 		return
 	})
 	defer dbhnlr.Dispose()
-	vm = func() (nvm *active.VM) {
-		nvm = active.NewVM()
-		nvm.ErrPrint = func(a ...interface{}) (vmerr error) {
-			if Out != nil {
-				Out.Print("<pre>ERR:\r\n")
-				Out.Print(a...)
-				Out.Print("\r\n</pre>")
-			}
-			return
+	invokevm := func() *active.VM {
+		if vm != nil {
+			return vm
 		}
-		nvm.Set("fs", fs)
-		nvm.Set("listen", LISTEN)
-		nvm.Set("lstn", LISTEN)
-		if terminal == nil {
-			terminal = newTerminal()
-		}
-		nvm.Set("terminal", terminal)
-		nvm.Set("trm", terminal)
-		nvm.Set("command", terminal)
-		nvm.Set("cmd", terminal)
-		nvm.Set("faf", func(rqstpath string) {
-			go ProcessRequestPath(rqstpath, nil)
-		})
-
-		var fparseEval = func(prsout io.Writer, evalrt interface{}, a ...interface{}) (prsevalerr error) {
-			var invert bool = false
-			if len(a) > 0 {
-				if inv, invok := a[0].(bool); invok {
-					invert = inv
-					a = a[1:]
+		vm = func() (nvm *active.VM) {
+			select {
+			case nvm = <-chnvms:
+				if nvm == nil {
+					nvm = active.NewVM()
 				}
+			default:
+				nvm = active.NewVM()
 			}
-			var prin, _ = evalrt.(io.Reader)
-			var suggestedroot = "/"
-			if fi != nil {
-				suggestedroot = fi.PathRoot()
-			}
-			var evalroot, _ = evalrt.(string)
-			if prsout == nil {
-				prsout = Out
-			} else if prsout != Out {
-				if nvm.W == Out {
-					nvm.SetPrinter(prsout)
-					defer func() {
-						nvm.SetPrinter(Out)
-					}()
+			nvm.ErrPrint = func(a ...interface{}) (vmerr error) {
+				if Out != nil {
+					Out.Print("<pre>ERR:\r\n")
+					Out.Print(a...)
+					Out.Print("\r\n</pre>")
 				}
+				return
 			}
-			if strings.HasSuffix(evalroot, "/") {
-				for _, evlpth := range []string{"index.html", "index.js"} {
-					if fis := fs.LS(evalroot + evlpth); len(fis) == 1 {
-						prsevalerr = vmParseEval(nvm, fis[0].Path(), ".js", fis[0].ModTime(), prsout, nil, fs, invert, fis[0], nil, nil)
-						return
+			nvm.Set("fs", fs)
+			nvm.Set("listen", LISTEN)
+			nvm.Set("lstn", LISTEN)
+			if terminal == nil {
+				terminal = newTerminal()
+			}
+			nvm.Set("terminal", terminal)
+			nvm.Set("trm", terminal)
+			nvm.Set("command", terminal)
+			nvm.Set("cmd", terminal)
+			nvm.Set("faf", func(rqstpath string) {
+				go ProcessRequestPath(rqstpath, nil)
+			})
+			var fparseEval = func(prsout io.Writer, evalrt interface{}, a ...interface{}) (prsevalerr error) {
+				var invert bool = false
+				var fitouse = fi
+				if len(a) > 0 {
+					if inv, invok := a[0].(bool); invok {
+						invert = inv
+						a = a[1:]
 					}
 				}
-			}
-			if fis := fs.LS(evalroot + ".js"); len(fis) == 1 {
-				prsevalerr = vmParseEval(nvm, fis[0].Path(), ".js", fis[0].ModTime(), prsout, nil, fs, invert, fis[0], nil, nil)
-			} else if len(fis) == 0 {
-				if evalroot != "" || (evalroot == "" && evalrt != nil) {
-					a = append([]interface{}{evalrt}, a...)
+				if prsout == nil {
+					prsout = Out
+				} else if prsout != Out {
+					if nvm.W == Out {
+						nvm.SetPrinter(prsout)
+						defer func() {
+							nvm.SetPrinter(Out)
+						}()
+					}
 				}
-				if prin == nil && len(a) > 0 {
-					func() {
-						defer prsevalbuf.Clear()
-						if prsevalbuf == nil {
-							prsevalbuf = iorw.NewBuffer()
-							prsevalbuf.Print(a...)
-						} else {
-							prsevalbuf.Clear()
-							prsevalbuf.Print(a...)
+				var evalroot, _ = evalrt.(string)
+				if evalroot != "" {
+					if fios := fs.LS(evalroot); len(fios) == 1 {
+						fitouse = fios[0]
+						evalroot = fitouse.PathRoot()
+					}
+				}
+				var prin, _ = evalrt.(io.Reader)
+				var suggestedroot = "/"
+				if fitouse != nil {
+					suggestedroot = fitouse.PathRoot()
+					if fitouse.IsDir() {
+						if strings.HasSuffix(evalroot, "/") {
+							for _, evlpth := range []string{"index.html", "index.js"} {
+								if fis := fs.LS(evalroot + evlpth); len(fis) == 1 {
+									prsevalerr = ParseEval(nvm.Eval, fis[0].Path(), ".js", fis[0].ModTime(), prsout, nil, fs, invert, fis[0], nil, nil)
+									return
+								}
+							}
 						}
-						if prsevalbuf.Size() > 0 {
-							prsevalerr = vmParseEval(nvm, ":no-cache/"+suggestedroot, ".js", time.Now(), prsout, prsevalbuf.Clone(true).Reader(true), fs, invert, nil, nil, nil)
+						return
+					}
+					prsevalerr = ParseEval(nvm.Eval, fitouse.Path(), fitouse.PathExt(), fitouse.ModTime(), prsout, nil, fs, invert, fitouse, nil, nil)
+					return
+				}
+
+				if fis := fs.LS(evalroot + ".js"); len(fis) == 1 {
+					prsevalerr = ParseEval(nvm.Eval, fis[0].Path(), ".js", fis[0].ModTime(), prsout, nil, fs, invert, fis[0], nil, nil)
+				} else if len(fis) == 0 {
+					if evalroot != "" || (evalroot == "" && evalrt != nil) {
+						a = append([]interface{}{evalrt}, a...)
+					}
+					if prin == nil && len(a) > 0 {
+						func() {
+							defer prsevalbuf.Clear()
+							if prsevalbuf == nil {
+								prsevalbuf = iorw.NewBuffer()
+								prsevalbuf.Print(a...)
+							} else {
+								prsevalbuf.Clear()
+								prsevalbuf.Print(a...)
+							}
+							if prsevalbuf.Size() > 0 {
+								prsevalerr = ParseEval(nvm.Eval, ":no-cache/"+suggestedroot, ".js", time.Now(), prsout, prsevalbuf.Clone(true).Reader(true), fs, invert, nil, nil, nil)
+							}
+						}()
+					} else {
+						if fitouse.PathRoot() != suggestedroot && fitouse.PathExt() != ".js" {
+							prsevalerr = ParseEval(nvm.Eval, ":no-cache/"+suggestedroot, ".js", time.Now(), prsout, prin, fs, invert, nil, nil, nil)
 						}
-					}()
-				} else {
-					prsevalerr = vmParseEval(nvm, ":no-cache/"+suggestedroot, ".js", time.Now(), prsout, prin, fs, invert, nil, nil, nil)
+					}
 				}
+				return prsevalerr
 			}
-			return prsevalerr
-		}
 
-		nvm.Set("parseEval", fparseEval)
+			nvm.Set("parseEval", fparseEval)
 
-		nvm.Set("scheduling", SCHEDULING)
-		nvm.Set("schdlng", SCHEDULING)
-		nvm.Set("caching", CHACHING)
-		nvm.Set("cchng", CHACHING)
-		nvm.Set("db", dbhnlr)
-		nvm.Set("dbqry", func(alias string, a ...interface{}) (reader *database.Reader) {
-			a = append([]interface{}{nvm}, a...)
+			nvm.Set("scheduling", SCHEDULING)
+			nvm.Set("schdlng", SCHEDULING)
+			nvm.Set("caching", CHACHING)
+			nvm.Set("cchng", CHACHING)
+			nvm.Set("db", dbhnlr)
+			nvm.Set("dbqry", func(alias string, a ...interface{}) (reader *database.Reader) {
+				a = append([]interface{}{nvm}, a...)
 
-			if params != nil {
-				a = append(a, params)
-			}
-			if fs != nil {
-				a = append(a, fs)
-			}
-			if reader = DBMS.Query(alias, a...); reader != nil {
-				reader.EventClose = func(r *database.Reader) {
-					dbclsrs.clsrs.Delete(r)
+				if params != nil {
+					a = append(a, params)
 				}
-				dbclsrs.clsrs.Store(reader, reader)
-			}
-			return
-		})
-
-		nvm.Set("dbexec", func(alias string, a ...interface{}) (exectr *database.Executor) {
-			a = append([]interface{}{nvm}, a...)
-
-			if params != nil {
-				a = append(a, params)
-			}
-			if fs != nil {
-				a = append(a, fs)
-			}
-			if exectr = DBMS.Execute(alias, a...); exectr != nil {
-				exectr.EventClose = func(ex *database.Executor) {
-					dbclsrs.clsrs.Delete(ex)
+				if fs != nil {
+					a = append(a, fs)
 				}
-				dbclsrs.clsrs.Store(exectr, exectr)
-			}
-			return
-		})
-
-		nvm.Set("dbreg", func(alias string, driver string, datasource string, a ...interface{}) bool {
-			return DBMS.Register(alias, driver, datasource, a...)
-		})
-
-		nvm.Set("dbunreg", func(alias string, a ...interface{}) bool {
-			return DBMS.Unregister(alias, a...)
-		})
-
-		nvm.Set("dbprep", func(alias string, a ...interface{}) (exectr *database.Executor) {
-			a = append([]interface{}{nvm}, a...)
-
-			if params != nil {
-				a = append(a, params)
-			}
-			if fs != nil {
-				a = append(a, fs)
-			}
-			if exectr = DBMS.Prepair(alias, a...); exectr != nil {
-				exectr.EventClose = func(ex *database.Executor) {
-					dbclsrs.clsrs.Delete(ex)
+				if reader = DBMS.Query(alias, a...); reader != nil {
+					reader.EventClose = func(r *database.Reader) {
+						dbclsrs.clsrs.Delete(r)
+					}
+					dbclsrs.clsrs.Store(reader, reader)
 				}
-				dbclsrs.clsrs.Store(exectr, exectr)
+				return
+			})
+
+			nvm.Set("dbexec", func(alias string, a ...interface{}) (exectr *database.Executor) {
+				a = append([]interface{}{nvm}, a...)
+
+				if params != nil {
+					a = append(a, params)
+				}
+				if fs != nil {
+					a = append(a, fs)
+				}
+				if exectr = DBMS.Execute(alias, a...); exectr != nil {
+					exectr.EventClose = func(ex *database.Executor) {
+						dbclsrs.clsrs.Delete(ex)
+					}
+					dbclsrs.clsrs.Store(exectr, exectr)
+				}
+				return
+			})
+
+			nvm.Set("dbreg", func(alias string, driver string, datasource string, a ...interface{}) bool {
+				return DBMS.Register(alias, driver, datasource, a...)
+			})
+
+			nvm.Set("dbunreg", func(alias string, a ...interface{}) bool {
+				return DBMS.Unregister(alias, a...)
+			})
+
+			nvm.Set("dbprep", func(alias string, a ...interface{}) (exectr *database.Executor) {
+				a = append([]interface{}{nvm}, a...)
+
+				if params != nil {
+					a = append(a, params)
+				}
+				if fs != nil {
+					a = append(a, fs)
+				}
+				if exectr = DBMS.Prepair(alias, a...); exectr != nil {
+					exectr.EventClose = func(ex *database.Executor) {
+						dbclsrs.clsrs.Delete(ex)
+					}
+					dbclsrs.clsrs.Store(exectr, exectr)
+				}
+				return
+			})
+
+			nvm.Set("email", EMAILING.ActiveEmailManager(nvm, func() parameters.ParametersAPI {
+				return params
+			}, fs))
+			for actvkey, actvval := range activemap {
+				nvm.Set(actvkey, actvval)
 			}
-			return
-		})
 
-		nvm.Set("email", EMAILING.ActiveEmailManager(nvm, func() parameters.ParametersAPI {
-			return params
-		}, fs))
-		for actvkey, actvval := range activemap {
-			nvm.Set(actvkey, actvval)
-		}
+			var vmparam = map[string]interface{}{
+				"set":       params.SetParameter,
+				"get":       params.Parameter,
+				"exist":     params.ContainsParameter,
+				"fileExist": params.ContainsFileParameter,
+				"setFile":   params.SetFileParameter,
+				"getFile":   params.FileParameter,
+				"keys":      params.StandardKeys,
+				"fileKeys":  params.FileKeys,
+				"fileName":  params.FileName,
+			}
 
-		var vmparam = map[string]interface{}{
-			"set":       params.SetParameter,
-			"get":       params.Parameter,
-			"exist":     params.ContainsParameter,
-			"fileExist": params.ContainsFileParameter,
-			"setFile":   params.SetFileParameter,
-			"getFile":   params.FileParameter,
-			"keys":      params.StandardKeys,
-			"fileKeys":  params.FileKeys,
-			"fileName":  params.FileName,
-		}
+			nvm.Set("param", vmparam)
+			nvm.Set("_in", In)
+			nvm.Set("_out", Out)
+			nvm.R = In
+			nvm.W = Out
 
-		nvm.Set("param", vmparam)
-		nvm.Set("_in", In)
-		nvm.Set("_out", Out)
-		nvm.R = In
-		nvm.W = Out
-
-		return nvm
-	}()
-
+			return nvm
+		}()
+		return vm
+	}
 	var rangeOffset = In.RangeOffset()
 	var rangeType = In.RangeType()
 	defer func() {
 		if vm != nil {
 			chnvms <- vm
-			vm = nil
 		}
 	}()
 
@@ -366,7 +379,7 @@ func internalServeRequest(path string, In *reader, Out *writer, httpw http.Respo
 	var invertactive = false
 	if strings.Contains(path, "/active:") {
 		for strings.Contains(path, "/active:") {
-			prepath := path[:strings.Index(path, "/active:")]
+			prepath := path[:strings.Index(path, "/active:")+1]
 			path = prepath + path[strings.Index(path, "/active:")+len("/active:"):]
 		}
 		invertactive = true
@@ -417,7 +430,7 @@ func internalServeRequest(path string, In *reader, Out *writer, httpw http.Respo
 			}
 
 			if !israw && isactive {
-				err = vmParseEval(vm, path, pathext, pathmodified, Out, nil, fs, invertactive, fi, fnmodified, fnactiveraw)
+				err = ParseEval(invokevm().Eval, path, pathext, pathmodified, Out, nil, fs, invertactive, fi, fnmodified, fnactiveraw)
 			} else if israw || ismedia {
 				if ismedia {
 					if f, ferr := fi.Open(); ferr == nil {
