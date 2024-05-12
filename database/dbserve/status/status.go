79c6b544a0865fab0b899792f323cd1dddd68d05
evocert <evocert@gmail.com>
2024-05-12 07:09:43 +0200

    Iplementation of "status" dbserve.AliasCommandFunc API caller

---
+++ database/dbserve/status/status.go
@@ -0,0 +1,48 @@
+package status
+
+import (
+	"encoding/json"
+
+	"github.com/evocert/lnksnk/database"
+	"github.com/evocert/lnksnk/database/dbserve"
+	"github.com/evocert/lnksnk/fsutils"
+	"github.com/evocert/lnksnk/serve/serveio"
+)
+
+var aliascmdstatus dbserve.AliasCommandFunc = func(alias, path, ext string, dbhnl *database.DBMSHandler, w serveio.Writer, r serveio.Reader, fs *fsutils.FSUtils) (err error) {
+	if prms := dbhnl.Params(); prms != nil {
+		var errorsfnd []interface{} = nil
+		var warnings []interface{} = nil
+		isalias := alias != ""
+		if isalias {
+			sts, stserr := dbhnl.Status(alias)
+			if stserr == nil {
+				if len(sts) > 0 {
+					enc := json.NewEncoder(w)
+					enc.Encode(sts)
+					return
+				}
+				warnings = append(warnings, "No status info")
+			} else if stserr != nil {
+				errorsfnd = append(errorsfnd, "Status-err:"+stserr.Error())
+			}
+		}
+		if !isalias {
+			errorsfnd = append(errorsfnd, "No alias provided")
+		}
+		enc := json.NewEncoder(w)
+		mp := map[string]interface{}{}
+		if len(errorsfnd) > 0 {
+			mp["err"] = errorsfnd
+		}
+		if len(warnings) > 0 {
+			mp["warn"] = warnings
+		}
+		err = enc.Encode(mp)
+	}
+	return
+}
+
+func init() {
+	dbserve.HandleCommand("status", aliascmdstatus)
+}
