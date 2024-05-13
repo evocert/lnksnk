5cc410ff3ee65c30aa215b5a015e01e446dc4eb0
evocert <evocert@gmail.com>
2024-05-13 07:29:31 +0200

    sys.go can run as service or app if the first arg in the console is 'app'

---
+++ sys/sys.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"strings"
+
+	//_ "github.com/evocert/lnksnk/database/mssql"
+	//_ "github.com/evocert/lnksnk/database/mysql"
+	//_ "github.com/evocert/lnksnk/database/ora"
+	_ "github.com/evocert/lnksnk/database/dbserve/connections"
+	_ "github.com/evocert/lnksnk/database/dbserve/drivers"
+	_ "github.com/evocert/lnksnk/database/dbserve/exec"
+	_ "github.com/evocert/lnksnk/database/dbserve/query"
+	_ "github.com/evocert/lnksnk/database/dbserve/register"
+	_ "github.com/evocert/lnksnk/database/dbserve/status"
+	_ "github.com/evocert/lnksnk/database/postgres"
+	_ "github.com/evocert/lnksnk/fonts"
+	"github.com/evocert/lnksnk/sys/app"
+	_ "github.com/evocert/lnksnk/sys/app"
+	"github.com/evocert/lnksnk/sys/srv"
+	_ "github.com/evocert/lnksnk/sys/srv"
+	_ "github.com/evocert/lnksnk/ui"
+)
+
+func main() {
+
+	args := os.Args
+	if len(args) > 1 {
+		if strings.EqualFold(args[1], "app") {
+			args = append(args[:1], args[1:]...)
+			app.App(args...)
+			return
+		}
+	}
+	srv.Serve(os.Args...)
+}
